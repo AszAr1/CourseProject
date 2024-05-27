22d276269c2c87fd8506a85a7941d73806584660
AszAr1 <aszaslavagutkovski1605@gmail.com>
2024-05-27 18:42:31 +0300

    added and implemeted MovieInfoService

---
+++ backend/Services/MovieInfo/IMovieInfoService.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+
+namespace Kinopoisk.Services.MovieInfo;
+
+public interface IMovieInfoService {
+    Task<List<GetMovieInfoDTO>> GetMovieInfos();
+    Task<GetMovieInfoDTO> GetMovieInfo(string name);
+    Task<GetMovieInfoDTO> AddMovieInfo(CreateMovieInfoDTO createMovieInfoDTO);
+    Task<GetMovieInfoDTO> UpdateMovieInfo(UpdateMovieInfoDTO updateMovieInfoDTO);
+    Task<Hashtable> DeleteMovieInfo(string name);   
+}
---
+++ backend/Services/MovieInfo/MovieInfoService.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+
+namespace Kinopoisk.Services.MovieInfo;
+
+public class MovieInfoService : IMovieInfoService {
+    private readonly DataContext context;
+    private IMapper mapper;
+    public MovieInfoService(DataContext context, IMapper mapper) {
+        this.context = context;
+        this.mapper = mapper;
+    }
+    public async Task<GetMovieInfoDTO> AddMovieInfo(CreateMovieInfoDTO createMovieInfoDTO) {
+        var movieInfo =  mapper.Map<MovieInfoModel>(createMovieInfoDTO);
+        context.MovieInfos.Add(movieInfo);
+        await context.SaveChangesAsync();
+        return mapper.Map<GetMovieInfoDTO>(movieInfo); 
+    }
+
+    public Task<Hashtable> DeleteMovieInfo(string name) {
+        throw new NotImplementedException();
+    }
+
+    public async Task<GetMovieInfoDTO> GetMovieInfo(string name) {
+        var movieInfo = await context.MovieInfos.FirstOrDefaultAsync(info => info.NameEn == name);
+        return mapper.Map<GetMovieInfoDTO>(movieInfo);
+    }
+
+    public async Task<List<GetMovieInfoDTO>> GetMovieInfos() {
+        var movieInfos = await context.MovieInfos.ToListAsync();
+        return movieInfos.Select(mapper.Map<GetMovieInfoDTO>).ToList();
+    }
+
+    public Task<GetMovieInfoDTO> UpdateMovieInfo(UpdateMovieInfoDTO updateMovieInfoDTO) {
+        throw new NotImplementedException();
+    }
+}
