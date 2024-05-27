cfeb3bfaa1f0d3bd6af778bf669f820c63173010
AszAr1 <aszaslavagutkovski1605@gmail.com>
2024-05-27 18:58:43 +0300

    added new imports, new scopes for services, cors, jwt authentication, swagger gen

--- backend/Program.cs
+++ backend/Program.cs
@@ -2,20 +2,30 @@ global using Kinopoisk.Models;
 global using Kinopoisk.Services.Movie;
 global using Kinopoisk.Services.Country;
 global using Kinopoisk.Services.Genre;
+global using Kinopoisk.Services.MovieInfo;
+global using Kinopoisk.Services.User;
 global using Kinopoisk.DTOs.Movie;
 global using Kinopoisk.DTOs.MovieInfo;
 global using Kinopoisk.DTOs.Country;
 global using Kinopoisk.DTOs.Genre;
+global using Kinopoisk.DTOs.User;
 global using Kinopoisk.Data;
 global using Kinopoisk.Enums;
+global using Kinopoisk.Json;
+global using Kinopoisk.Helpers;
 
 using Microsoft.Extensions.FileProviders;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 
 string connectionString = builder.Configuration.GetConnectionString("Default")!;
+string MyAllowSpecificOrigins = "myAllowSpecificOrigins";
 builder.Services.AddDbContext<DataContext>(
     options => options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString))
 );
@@ -27,6 +37,62 @@ builder.Services.AddAutoMapper(typeof(Program).Assembly);
 builder.Services.AddScoped<IMovieService, MovieService>();
 builder.Services.AddScoped<ICountryService, CountryService>();
 builder.Services.AddScoped<IGenreService, GenreService>();
+builder.Services.AddScoped<IMovieInfoService, MovieInfoService>();
+builder.Services.AddScoped<IUserService, UserService>();
+
+builder.Services.AddCors(
+    options => {
+        options.AddPolicy(
+            name: MyAllowSpecificOrigins,
+            policy => {
+                policy.WithOrigins("http://localhost:5173", "http://localhost:5174");
+            }
+        );
+    }
+);
+builder.Services.AddAuthentication(cfg => {
+    cfg.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    cfg.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    cfg.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(x => {
+    x.RequireHttpsMetadata = false;
+    x.SaveToken = false;
+    x.TokenValidationParameters = new TokenValidationParameters {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(
+            Encoding.UTF8.GetBytes(builder.Configuration["ApplicationSettings:JWT_Secret"]!)
+        ),
+        ValidateIssuer = false,
+        ValidateAudience = false,
+        ClockSkew = TimeSpan.Zero,
+    };
+});
+builder.Services.AddSwaggerGen(swagger => {
+    swagger.SwaggerDoc("v1", new OpenApiInfo {
+        Version = "v1",
+        Title = "JWT Token Authentication API",
+        Description = ".NET 8 Web API"
+    });
+    swagger.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme() {
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "JWT Authorization header using the Bearer scheme.\r\nEnter 'Bearer' [space] and then your token in the text input below.\r\nExample: \"Bearer 12345abcdef\"",
+    });
+    swagger.AddSecurityRequirement(new OpenApiSecurityRequirement {
+        {
+            new OpenApiSecurityScheme {
+                Reference = new OpenApiReference {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            new string[] {}
+        }
+    });
+});
 
 var app = builder.Build();
 
@@ -36,12 +102,16 @@ if (app.Environment.IsDevelopment()) {
 }
 
 app.UseHttpsRedirection();
+app.UseAuthentication();
+app.UseAuthorization();
+app.UseMiddleware<JwtMiddleware>();
 app.UseStaticFiles(new StaticFileOptions
 {
     FileProvider = new PhysicalFileProvider(
            Path.Combine(builder.Environment.ContentRootPath, "assets")),
     RequestPath = "/assets"
 });
+app.UseCors(MyAllowSpecificOrigins);
 
 app.MapControllers();
 
