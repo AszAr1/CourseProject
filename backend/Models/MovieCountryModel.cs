9f764997d548bac92d911037b9b0c618d975fb80
AszAr1 <aszaslavagutkovski1605@gmail.com>
2024-06-02 16:32:12 +0300

    removed unnecessary ids from MovieGenreModel and MovieCountryModel

--- backend/Models/MovieCountryModel.cs
+++ backend/Models/MovieCountryModel.cs
@@ -1,13 +1,12 @@
 using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 namespace Kinopoisk.Models;
 
+[Keyless]
 [Table("movies_countries")]
 public class MovieCountryModel {
-    [Column("id")]
-    public Guid Id { get; set; } = Guid.NewGuid();
-
     [Column("movie_info_id")]
     [ForeignKey(nameof(MovieInfo))]
     public Guid MovieInfoId { get; set; }
