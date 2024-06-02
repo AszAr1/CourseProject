739b6200e5614041c5f8edd0f61c56bf9e5f90d1
AszAr1 <aszaslavagutkovski1605@gmail.com>
2024-06-02 16:46:50 +0300

    updated AutoMappeProfile

--- backend/AutoMapperProfile.cs
+++ backend/AutoMapperProfile.cs
@@ -5,13 +5,12 @@ namespace Kinopoisk;
 public class AutoMapperProfile : Profile {
     public AutoMapperProfile() {
         CreateMap<MovieModel, GetMovieDTO>();
-        CreateMap<CreateMovieDTO, MovieModel>();
-        CreateMap<UpdateMovieDTO, MovieModel>();
+        CreateMap<CreateUpdateMovieDTO, MovieModel>();
 
-        CreateMap<CreateMovieInfoDTO, MovieInfoModel>();
+        CreateMap<CreateUpdateMovieInfoDTO, MovieInfoModel>();
         CreateMap<MovieInfoModel, GetMovieInfoDTO>();
 
-        CreateMap<CountryDTO, CountryModel>();
+        CreateMap<CountryDTO, CountryModel>(); 
         CreateMap<CountryModel, CountryDTO>();
 
         CreateMap<GenreDTO, GenreModel>();
