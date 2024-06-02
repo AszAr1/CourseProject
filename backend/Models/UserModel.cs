38faaf31f2f9fe21b80f658fc02ee3f07ee29429
AszAr1 <aszaslavagutkovski1605@gmail.com>
2024-06-02 17:00:44 +0300

    added default values to the boolean fields and list of WatchedMovieModel

--- backend/Models/UserModel.cs
+++ backend/Models/UserModel.cs
@@ -13,8 +13,11 @@ public class UserModel {
     [Column("password_digest")]
     public string Password { get; set; } = "";
     [Column("is_admin")]
-    public bool IsAdmin { get; set; }
+    public bool IsAdmin { get; set; } = false;
     [Column("is_authorized")]
-    public bool IsAuthorized { get; set; }
-    public bool isActive { get; set; }
+    public bool IsAuthorized { get; set; } = false;
+    [Column("is_active")]
+    public bool isActive { get; set; } = true;
+
+    public List<WatchedMovieModel> watchedMovies { get; set; } = new List<WatchedMovieModel>();
 }
